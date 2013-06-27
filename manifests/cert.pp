3f1649ed352ba1de0885f06aac2d94c5252ea042
Sam Morrison <sorrison@gmail.com>
2013-06-27 11:40:14 +1000

    Remove NFS stuff from nova cert
    Change-Id: I7fb93c9e8bf35fe67dcca5fd76bd5db9e745d5ce

--- manifests/cert.pp
+++ manifests/cert.pp
@@ -6,17 +6,6 @@ class nova::cert {
     ensure  => installed,
   }
 
-  mount { '/var/lib/nova/CA':
-    device  => $nova_cc_nfs_ca_mount,
-    fstype  => 'nfs',
-    ensure  => mounted,
-    options => '_netdev,auto',
-    atboot  => true,
-    notify  => Service['nova-cert'],
-    require => [ Package['nfs-common'],
-                 Package['nova-cert']],
-  }
-
   file { '/var/lib/nova/CA/openssl.cnf':
     ensure  => present,
     owner   => nova,
@@ -24,7 +13,6 @@ class nova::cert {
     mode    => '0644',
     source  => 'puppet:///modules/nova/openssl.cnf',
     notify  => Service['nova-cert'],
-    require => Mount['/var/lib/nova/CA'],
   }
 
   service { 'nova-cert':
