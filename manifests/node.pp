86bfc67211ad35c0fbaa603529bed2c309073884
Sam Morrison <sorrison@gmail.com>
2014-01-30 10:21:27 +1100

    Move nova-api-metadata into own class
    Change-Id: I8b42905848617fd25f7a5b7e477dfae4c2e06eab

--- manifests/node.pp
+++ manifests/node.pp
@@ -86,21 +86,4 @@ class nova::node (
     require => Package['nova-api-metadata'],
   }
 
-  package { 'nova-api-metadata':
-    ensure  => present,
-    require => User['nova'],
-  }
-
-  service { 'nova-api-metadata':
-    ensure    => running,
-    enable    => true,
-    provider  => upstart,
-    subscribe => File['/etc/nova/nova.conf'],
-    require   => Package['nova-api-metadata'],
-  }
-
-  nagios::nrpe::service {
-    'service_nova_api_metadata':
-      check_command => '/usr/lib/nagios/plugins/check_procs -c 1:1 -u nova -a /usr/bin/nova-api-metadata';
-  }
 }
