2a580a39e223aba354382ad3ee43284c9a922eba
Sam Morrison <sorrison@gmail.com>
2014-02-04 14:10:15 +1100

    Push out rootwrap for compute to support cp command
    Change-Id: Id1e975d5543b0741c761443a3222052f0ff39b64

--- manifests/compute.pp
+++ manifests/compute.pp
@@ -15,6 +15,15 @@ class nova::compute {
                    File['/etc/nova/nova.conf']],
   }
 
+  file { '/etc/nova/rootwrap.d/compute.filters':
+    ensure  => present,
+    owner   => nova,
+    group   => nova,
+    mode    => '0600',
+    source  => "puppet:///modules/nova/${openstack_version}/compute.filters",
+    require => Package['nova-common'],
+  }
+
   nagios::nrpe::service {
     'service_nova_compute':
       check_command => '/usr/lib/nagios/plugins/check_procs -c 1:1 -u nova -a /usr/bin/nova-compute';
