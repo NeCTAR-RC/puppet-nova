43887ab9064a01ec19796208af74e4e40e578179
Sam Morrison <sorrison@gmail.com>
2014-02-04 16:36:50 +1100

    Fix up some dependency loops
    Change-Id: I39287bcaeaa1ac9687533f5030078f7499953ad7

--- manifests/api-metadata.pp
+++ manifests/api-metadata.pp
@@ -2,6 +2,12 @@ class nova::api-metadata {
 
   require nova::node
 
+  $openstack_version = hiera('openstack_version')
+  $keystone_host = hiera('keystone::host')
+  $keystone_protocol = hiera('keystone::protocol')
+  $keystone_service_tenant = hiera('keystone::service_tenant')
+  $cell_config = hiera_hash('nova::cell_config')
+
   package { 'nova-api-metadata':
     ensure  => present,
     require => User['nova'],
@@ -13,6 +19,16 @@ class nova::api-metadata {
     provider  => upstart,
     subscribe => File['/etc/nova/nova.conf'],
     require   => Package['nova-api-metadata'],
+  }
+
+  file { '/etc/nova/api-paste.ini':
+    ensure  => present,
+    owner   => nova,
+    group   => nova,
+    mode    => '0640',
+    content => template("nova/${openstack_version}/api-paste.ini.erb"),
+    notify  => Service['nova-api-metadata'],
+    require => Package['nova-api-metadata'],
   }
 
   nagios::nrpe::service {
--- manifests/compute.pp
+++ manifests/compute.pp
@@ -21,11 +21,30 @@ class nova::compute {
     group   => nova,
     mode    => '0600',
     source  => "puppet:///modules/nova/${openstack_version}/compute.filters",
-    require => Package['nova-common'],
+    require => Package['nova-compute'],
   }
 
   nagios::nrpe::service {
     'service_nova_compute':
       check_command => '/usr/lib/nagios/plugins/check_procs -c 1:1 -u nova -a /usr/bin/nova-compute';
+  }
+
+  $instance_mount = hiera('nova::node::instance_mount', undef)
+
+  if $instances_mount {
+
+    package { 'nfs-common':
+      ensure => installed,
+    }
+
+    mount { '/var/lib/nova/instances':
+      device  => $instances_mount,
+      fstype  => hiera('nfs::type', 'nfs'),
+      ensure  => mounted,
+      options => hiera('nfs::options', '_netdev,auto'),
+      atboot  => true,
+      require => [ Package['nova-compute'],
+                   Package['nfs-common']],
+    }
   }
 }
