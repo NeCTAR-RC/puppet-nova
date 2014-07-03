b47ac415ed79da9a8df349ea53433bd07c82b43d
Russell Sim <russell.sim@gmail.com>
2014-07-03 05:52:25 +0000

    Support multiple workers for nova cells
    Change-Id: I14093ec4d909c4e7b51841e3cbd17eae79552ef4

--- manifests/cells.pp
+++ manifests/cells.pp
@@ -1,4 +1,6 @@
-class nova::cells {
+class nova::cells (
+  $workers=1,
+) {
 
   require nova::cloudcontroller
 
@@ -14,6 +16,24 @@ class nova::cells {
 
   nagios::nrpe::service {
     'service_nova_cells':
-      check_command => '/usr/lib/nagios/plugins/check_procs -c 1:1 -u nova -a /usr/bin/nova-cells';
+      check_command => "/usr/lib/nagios/plugins/check_procs -c $workers:$workers -u nova -a /usr/bin/nova-cells";
+  }
+
+  define worker() {
+    file {"/etc/init/${name}.conf":
+      content => template('nova/nova-cells-init.conf.erb'),
+    }
+
+    service {"${name}":
+      ensure    => running,
+      provider  => upstart,
+      subscribe => File['/etc/nova/nova.conf'],
+      require   => [File["/etc/init/${name}.conf"], Package['nova-cells']],
+    }
+  }
+
+  if $workers > 1 {
+    $workers_array = range("nova-cells-1", "nova-cells-$workers")
+    worker { [$workers_array]: }
   }
 }
