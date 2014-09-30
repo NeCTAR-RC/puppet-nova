94826ca7600e3cc29555d5f03e3bedeb9c21ab8b
Sam Morrison <sorrison@gmail.com>
2014-09-30 16:31:39 +1000

    Havana and Icehouse have different return codes for novnc
    Change-Id: I6843fcdd12f943ed360c8a07dcfcf0fd0f111c8b

--- manifests/api.pp
+++ manifests/api.pp
@@ -45,6 +45,6 @@ class nova::api::nagios-checks {
     'check_ec2':
       check_command => '/usr/lib/nagios/plugins/check_http -u /services/Cloud/ -p \'$ARG1$\' -e 400 -H \'$HOSTADDRESS$\' -I \'$HOSTADDRESS$\'';
     'check_novnc':
-      check_command => '/usr/lib/nagios/plugins/check_http -p \'$ARG1$\' -e 404 -H \'$HOSTADDRESS$\' -I \'$HOSTADDRESS$\'';
+      check_command => '/usr/lib/nagios/plugins/check_http -p \'$ARG1$\' -e 200,404 -H \'$HOSTADDRESS$\' -I \'$HOSTADDRESS$\'';
   }
 }
