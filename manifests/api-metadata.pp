2bff610001723786ffb46b38ea20aa1fb5a32aa1
Sam Morrison <sorrison@gmail.com>
2017-01-12 11:22:11 +1100

    Remove deprecated classes
    Change-Id: I8da3e0e8b39b3e004c065663de551e519efadddb

--- manifests/api-metadata.pp
+++
@@ -1,6 +0,0 @@
-# Keep backwards compatability. Remove when everyone migrates to the new class
-class nova::api-metadata {
-    include ::nova::api::metadata
-
-    notify {'class nova::api-metadata is deprecated. Please use nova::api::metadata': }
-}
