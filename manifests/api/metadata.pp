82b7ea71fea94a38a41b199521bc444959169b44
Jake Yip <jake.yip@unimelb.edu.au>
2017-01-25 14:47:14 +1100

    Manage rootwrap.d directory
    for api nodes
    
    Change-Id: If4604a12ce768bbcb07173934f30773babd7e4e3

--- manifests/api/metadata.pp
+++ manifests/api/metadata.pp
@@ -14,6 +14,17 @@ class nova::api::metadata {
     require   => Package['nova-api-metadata'],
   }
 
+  file { '/etc/nova/rootwrap.d':
+    ensure  => directory,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    recurse => true,
+    purge   => true,
+    force   => true,
+    require => Package['nova-common'],
+  }
+
   file {'/etc/nova/rootwrap.d/api-metadata.filters':
     ensure  => present,
     owner   => 'root',
