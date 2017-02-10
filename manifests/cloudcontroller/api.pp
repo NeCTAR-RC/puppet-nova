a285492b606a4b33bd92ea710df69bc7b87a9378
Jake Yip <jake.yip@unimelb.edu.au>
2017-02-10 16:26:33 +1100

    Bugfix: Pull in openstack_version from hiera
    Change-Id: I154614ca31fc1334ff2bd7949d01ea077ebbaf71

--- manifests/cloudcontroller/api.pp
+++ manifests/cloudcontroller/api.pp
@@ -4,12 +4,13 @@ class nova::cloudcontroller::api(
   $metadata_workers = 1,
 ) inherits nova::cloudcontroller {
 
+  $openstack_version = hiera('openstack_version')
   $metadata_proxy_shared_secret = hiera(
     'neutron::agents::metadata::shared_secret', false
   )
 
   File['/etc/nova/nova.conf'] {
-    content => template("nova/${::openstack_version}/nova.conf-api.erb"),
+    content => template("nova/${openstack_version}/nova.conf-api.erb"),
   }
 
 }
