7c271d6ed19fcd793ce8ee140f9b7ae37ab26d0d
Sam Morrison <sorrison@gmail.com>
2019-11-12 16:33:33 +1100

    Add some more nags for unused cellsv1 variables
    Change-Id: I530f2141351e87f54c291a251a184ef6530fd5f0

--- manifests/node.pp
+++ manifests/node.pp
@@ -33,6 +33,25 @@ class nova::node (
     $novncproxy_base_url = "http://${vnc_host}:6080/vnc_auto.html"
   }
 
+  nag::unused_variable{'nova::use_conductor':}
+  nag::unused_variable{'nova::cell_type':}
+  nag::unused_variable{'nova::cell_name':}
+  $unused_cell_variables = ['nova::cell_config.use_cells',
+                            'nova::cell_config.cells_instance_updated_at_threshold',
+                            'nova::cell_config.cells_instance_update_num_instances',
+                            'nova::cell_config.cells_capabilities',
+                            'nova::cell_config.cells_expire_reservations',
+                            'nova::cell_config.cells_scheduler_direct_only_cells',
+                            'nova::cell_config.cells_heal_update_number',
+                            'nova::cell_config.capacity_aggregate_key',
+                            'nova::cell_config.reserve_percent',
+                            'nova::cell_config.free_disk_units_needed',
+                            'nova::cell_config.cells_enable',
+                            ]
+  nag::unused_variable{$unused_cell_variables:
+    merge_strategy => 'deep',
+  }
+
   include ::memcached::python
 
   package {'nova-common':
