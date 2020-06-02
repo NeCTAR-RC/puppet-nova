8d1e913868d6763f4faae570090c6e49660dfdd0
Sam Morrison <sorrison@gmail.com>
2020-06-02 14:26:39 +1000

    Add more nags for old variables
    Change-Id: I73bb58b95600fb56975b889273dc042c59fa6b57

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,6 +5,8 @@ class nova {
   nag::unused_variable{'nova::use_conductor':}
   nag::unused_variable{'nova::cell_type':}
   nag::unused_variable{'nova::cell_name':}
+  nag::unused_variable{'nova::cloudcontroller::cell_name':}
+
   $unused_cell_variables = ['nova::cell_config.use_cells',
                             'nova::cell_config.cells_instance_updated_at_threshold',
                             'nova::cell_config.cells_instance_update_num_instances',
@@ -16,6 +18,13 @@ class nova {
                             'nova::cell_config.reserve_percent',
                             'nova::cell_config.free_disk_units_needed',
                             'nova::cell_config.cells_enable',
+                            'nova::cell_config.cells_name',
+                            'nova::cell_config.cells_full_name',
+                            'nova::cell_config.rabbit_hosts',
+                            'nova::cell_config.rabbit_user',
+                            'nova::cell_config.rabbit_pass',
+                            'nova::cell_config.rabbit_virtual_host',
+                            'nova::cell_config.capacity_aggregate_key',
                             ]
   nag::unused_variable{$unused_cell_variables:
     merge_strategy => 'deep',
