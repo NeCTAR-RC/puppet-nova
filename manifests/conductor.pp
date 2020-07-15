dc808e02daaf3ca3fe74853cc0019c8918bde9a5
Shi Yan <shi.yan@unimelb.edu.au>
2020-07-16 00:16:55 +1000

    Move conductor/novnc nagios out of nova module
    Change-Id: I960679c89207ccf51ebd1fe54acf639d55ae4f3f

--- manifests/conductor.pp
+++ manifests/conductor.pp
@@ -14,13 +14,5 @@ class nova::conductor($workers=0) {
     subscribe => File['/etc/nova/nova.conf'],
     require   => Package['nova-conductor'],
   }
-  if $workers == 0 {
-    $cores = $::processorcount + 1
-  } else {
-    $cores = $workers
-  }
-  nagios::nrpe::service {
-    'service_nova_conductor':
-      check_command => "/usr/lib/nagios/plugins/check_procs -c ${cores}:${cores} -u nova -a /usr/bin/nova-conductor";
-  }
+
 }
