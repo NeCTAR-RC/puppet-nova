9c7205c00311ad3df8d78e713660bacb2fd9b10c
Shi Yan <shi.yan@unimelb.edu.au>
2020-07-16 00:16:55 +1000

    Move conductor/novnc nagios out of nova module
    Change-Id: I960679c89207ccf51ebd1fe54acf639d55ae4f3f

--- manifests/novnc.pp
+++ manifests/novnc.pp
@@ -18,16 +18,6 @@ class nova::novnc {
     ensure => installed,
   }
 
-  nagios::nrpe::service {
-    'service_nova_novncproxy':
-      check_command => '/usr/lib/nagios/plugins/check_procs -c 1:20 -w 1:10 -u nova -a /usr/bin/nova-novncproxy';
-  }
-
-  nagios::service {
-    'http_novncproxy':
-      check_command => 'check_novnc!6080';
-  }
-
   firewall { '100 novnc':
     dport  => 6080,
     proto  => tcp,
