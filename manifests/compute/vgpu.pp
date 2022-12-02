71b525742f5fd9436046146ccf7c062f107b43a1
Shi Yan <yanshi.403@gmail.com>
2022-12-02 15:18:29 +1100

    Use deprecated enabled_vgpu_types and device_addresses
    Similar to commits:
    abd937b4089a1f9b1568bd334040bad60b2b2e29
    19daf02d402889c0fd2ec93e008f054ab37346ca
    in nectar/xena branch.
    
    An upstream change in both nova[1] and puppet-nova[2] at Xena changed
    the config:
    'vgpu_${vgpu_type}/device_addresses' to 'mdev_${vgpu_type}/device_addresses',
    'devices/enabled_vgpu_types' to 'devices/enabled_mdev_types', then
    totally remove the vgpu class[3] at Yoga.
    
    This means that we cannot have <Xena version of compute nodes running
    when we upgrade to Yoga puppet module.
    
    Put back the old code so that we can continue supporting older compute
    nodes after we have upgraded puppet modules to Yoga.
    
    This patch can be removed when compute nodes are at Xena.
    
    [1] https://review.opendev.org/c/openstack/nova/+/801607
    [2] https://review.opendev.org/c/openstack/puppet-nova/+/810429
    [3] https://review.opendev.org/c/openstack/puppet-nova/+/834106
    
    Change-Id: I77a471839cc0196a91166195e35968f6e46d459b

---
+++ manifests/compute/vgpu.pp
@@ -0,0 +1,38 @@
+# Class nova::compute::vgpu
+#
+# DEPRECATED !!
+# Configures nova compute vgpu options
+#
+# === Parameters:
+#
+# [*vgpu_types_device_addresses_mapping*]
+#   (optional) Map of vgpu type(s) the instances can get as key and list of
+#   corresponding device addresses as value.
+#   Defaults to undef
+#
+class nova::compute::vgpu(
+  $vgpu_types_device_addresses_mapping = undef,
+) {
+  include nova::deps
+
+  include nova::compute::mdev
+
+  if !empty($vgpu_types_device_addresses_mapping) {
+    validate_legacy(Hash, 'validate_hash', $vgpu_types_device_addresses_mapping)
+    $vgpu_types_real = keys($vgpu_types_device_addresses_mapping)
+    nova_config {
+      'devices/enabled_vgpu_types': value => join(any2array($vgpu_types_real), ',');
+    }
+    $vgpu_types_device_addresses_mapping.each |$vgpu_type, $device_addresses| {
+      if !empty($device_addresses) {
+        nova_config {
+          "vgpu_${vgpu_type}/device_addresses": value => join(any2array($device_addresses), ',');
+        }
+      } else {
+        nova_config {
+          "vgpu_${vgpu_type}/device_addresses": ensure => absent;
+        }
+      }
+    }
+  }
+}
